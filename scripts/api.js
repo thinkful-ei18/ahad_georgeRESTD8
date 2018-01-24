c0d494fcd78b5ffaa00af96099a1efcfc181b6e3
Ahad Darkhosh <darkhosh@hotmail.com>
2018-01-24 11:39:19 -0800

    resolved an issue related to rendering and double logging out

--- scripts/api.js
+++ scripts/api.js
@@ -6,13 +6,11 @@ const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ahadgeorge';
   function getItems(callback) {
     // callback('api module works!');
-    $.getJSON(`${BASE_URL}/items`,callback);
+    const newLocal = $.getJSON(`${BASE_URL}/items`,callback);
 /* Call in TAs to look at line 9, and end of step 3 */
   }
   function createItem(name, callback) {
-    const newItem = {
-      name,
-    };
+    const newItem = {name};
     $.ajax({
       url: `${BASE_URL}/items`,
       method: 'POST',
--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,23 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems((items)=>{
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
 });
 
-store.items.push(Item.create('apples'));
+// store.items.push(Item.create('apples'));
 
-api.getItems(function(data) {
-  console.log(data);
-});
+// api.getItems(function(data) {
+//   console.log(data);
+// });
 
-// console.log(api.BASE_URL);
+// // console.log(api.BASE_URL);
 
-api.createItem('pears', (newItem) => {
+api.createItem('onions', (newItem) => {
   api.getItems((items) => {
     console.log(items);
+    shoppingList.render();
   });
 });
